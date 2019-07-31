e8fc4ee9bcb79ea202aa466b5b047f516edc6eba
Henry <henrysoverholt@gmail.com>
2019-07-31 08:42:47 -0400

    stuck but commiting

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,6 @@
 const contacts = [];
+document.querySelector("body").addEventListener("onload", displayContacts);
+
 function handleContacts(event) {
   event.preventDefault();
   contacts.unshift({
@@ -30,11 +32,46 @@ document.querySelector("form").addEventListener("submit", handleContacts);
 document
   .querySelector(".contactsListed")
   .addEventListener("click", handleDelete);
-
 function handleDelete(event) {
-  // console.log("delete button was clicked");
-  // console.dir(event.target.attributes[0].value);
-  contact.splice(Number(event.target.attributes[0].value), 1);
+  contacts.splice(Number(event.target.attributes[0].value), 1);
   displayContacts();
-  // console.log(event.target.attributes[0].value);
 }
+//I have no idea how to add code to part one to part two here.
+// class Contact {
+//   constructor(name, email, phone, relation) {
+//     this.name = name;
+//     this.email = email;
+//     this.phone = phone;
+//     this.relation = relation;
+//   }
+
+//   class AddressBook {
+//     constructor() {
+//       this.contacts = [];
+//     }
+//     addContacts(name, email, phone, relation) {
+//       this.contacts.unshift(new Contact(name, email, phone, relation));
+//     }
+//   };
+//   const addressBook = new AddressBook;
+//   addressBook.addContacts("Henry", "henry@gmail.com", "555-555-5555", "Myself");
+// addressBook.addContacts("Dad", "dad@gmail.com", "444-444-4444", "Dad");
+// addressBook.addContacts(
+//   "Brother",
+//   "brother@gmail.com",
+//   "333-333-3333",
+//   "Brother"
+// );
+// addressBook.addContacts("Mom", "mom@gmail.com", "222-222-2222", "Brother");
+// addressBook.addContacts(
+//   "Sister",
+//   "sister@gmail.com",
+//   "111-111-1111",
+//   "Brother"
+// );
+// addressBook.addContacts(
+//   "Steve",
+//   "steve@gmail.com",
+//   "666-666-6666",
+//   "Uncle-in-law"
+// );
